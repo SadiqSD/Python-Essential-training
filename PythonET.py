84a0fb78f53f926d087e789267d9d5a7eb5766e1
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-25 15:32:34 +0100

    nice

--- PythonET.py
+++ PythonET.py
@@ -1,2 +1,9 @@
 name = input("what's your name")
 print("i love python")
+
+
+print("this one that you're fighting, it is nonsecse")
+
+
+
+grapper chrom is a good thing to do and go to pycharm or python
