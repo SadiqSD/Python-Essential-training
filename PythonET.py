6837a68d4e1cc0cea7a1f893f325217ab337d31a
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-26 23:07:29 +0100

    Modified update

--- PythonET.py
+++ PythonET.py
@@ -1,9 +1,44 @@
-name = input("what's your name")
-print("i love python")
+# we want to invent something that can count how many words are there
+# in what you type
+
+# text = "a s f e g f f g"
+#
+# dic  = {}
+# for word in text.split():
+#     if word in dic:
+#         dic[word] += 1
+#     else:
+#         dic[word] = 1
+#
+# print(dic)
+# y = ("Time", 2, "Name", [2.4, "6"])
+# x = ("Time", 2, "Name", [2.4, "6"])
+# print(id(y[0]))
+# print(id(x[0]))
 
 
-print("this one that you're fighting, it is nonsecse")
+#a textarea that allows you to type a specific words. and the words are counted
+
+
+text = """
+my name is is my sadiq murtasla isa isa musa my name is sadiq isa musa
+"""
+
+import random
+
+user_guess = int(input("guess the input "))
+random_numer = random.randint(1,10)
+
+while user_guess != random_numer:
+    print
 
 
 
-grapper chrom is a good thing to do and go to pycharm or python
+
+
+
+
+
+
+
+
